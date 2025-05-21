5d370a13cccb10c0f62f5e1fceae2ca3efa140fb
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2025-05-22 01:11:31 +0530

    Exploring various http method routing

--- src/app.js
+++ src/app.js
@@ -3,8 +3,19 @@ const app = express();
 
 port = 7777;
 
-app.use("/api", (req, res) => {
+//!Order of middleware (routes) matters
+
+app.use("/api", (req, res) => { //use is a method to handle all HTTP methods.
     res.send('Hello from API');
+});
+
+app.get("/getapi", (req, res) => { //get is a method to handle GET requests.
+    res.send('Hello from API GET');
+});
+
+app.post("/postapi", (req, res) => { //post is a method to handle POST requests.
+    //We can write logic here to POST data to the server.
+    res.send('Hello from API POST');
 });
 
 app.use("/",(req, res)=>{
