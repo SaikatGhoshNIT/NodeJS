599311e3ed50acf34debb54fcf3d7806e840b20c
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2025-06-08 15:48:05 +0530

    connection request acceptance or rejection

--- src/routes/connectionRequestRouter.js
+++ src/routes/connectionRequestRouter.js
@@ -33,7 +33,7 @@ connectionRequestRouter.post(
       const existingRequest = await ConnectionRequest.findOne({
         $or: [
           {
-            fromUserId,
+            fromUserId : fromUserId,
             toUserId: toUserID,
           },
           {
@@ -76,5 +76,59 @@ connectionRequestRouter.post(
     }
   }
 );
+
+connectionRequestRouter.patch("/request/update/:status/:requestId", userAuth, async (req,res) =>{
+  try{
+    const { status, requestId } = req.params;
+    
+    const loginUserId = req.user._id;
+
+    // Validate the requestId and status
+    if(!requestId || !status){
+      return res.status(400).json({ error: "Request ID and status are required." });
+    }
+
+    // Validate the status
+    const validStatus = ["accepted", "rejected"];
+    const isValidStatus = validStatus.includes(status);
+    if (!isValidStatus){
+      return res.status(400).json({
+        error: "Invalid status. Status must be either 'accepted' or 'rejected'."
+      })
+    }
+
+    //Check if the requestID is valid
+    const connectionRequest = await ConnectionRequest.findOne({
+      _id: requestId,// Ensure the request ID is valid
+      toUserId: loginUserId, // Ensure the request belongs to the logged-in user
+      status: "interested" // Ensure the request is in 'interested' status only if connection request is already accepted or rejected, then it will not allow again.
+    })
+
+    if(!connectionRequest){
+      return res.status(404).json({ error: "Connection request not found or not in 'interested' status." });
+    }
+
+    // Check status to ensure it can only be updated once
+    /*if (connectionRequest.status === "accepted" || connectionRequest.status === "rejected") {
+      return res.status(400).json({
+        error: `Connection request has already been ${connectionRequest.status}.`
+      });
+    }*/
+    
+
+    // Update the connection request status
+    connectionRequest.status = status; // Set the new status
+    connectionRequest.updatedAt = Date.now(); // Update the timestamp
+    const data = await connectionRequest.save();
+    res.status(200).json({
+      message: `Connection request updated successfully to '${status}'.`,
+      data
+    });
+  }
+  catch(error){
+    console.error("Error updating connection request:", error);
+    res.status(500).send("Error updating connection request: " + error.message);
+  }
+})
 
 module.exports = connectionRequestRouter; // Export the connection request router
--- src/routes/dist/connectionRequestRouter.dev.js
+++ src/routes/dist/connectionRequestRouter.dev.js
@@ -120,4 +120,99 @@ connectionRequestRouter.post("/request/send/:status/:toUserID", userAuth, functi
     }
   }, null, null, [[0, 25]]);
 });
+connectionRequestRouter.patch("/request/update/:status/:requestId", userAuth, function _callee2(req, res) {
+  var _req$params2, status, requestId, loginUserId, validStatus, isValidStatus, connectionRequest, data;
+
+  return regeneratorRuntime.async(function _callee2$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          _context2.prev = 0;
+          _req$params2 = req.params, status = _req$params2.status, requestId = _req$params2.requestId;
+          loginUserId = req.user._id; // Validate the requestId and status
+
+          if (!(!requestId || !status)) {
+            _context2.next = 5;
+            break;
+          }
+
+          return _context2.abrupt("return", res.status(400).json({
+            error: "Request ID and status are required."
+          }));
+
+        case 5:
+          // Validate the status
+          validStatus = ["accepted", "rejected"];
+          isValidStatus = validStatus.includes(status);
+
+          if (isValidStatus) {
+            _context2.next = 9;
+            break;
+          }
+
+          return _context2.abrupt("return", res.status(400).json({
+            error: "Invalid status. Status must be either 'accepted' or 'rejected'."
+          }));
+
+        case 9:
+          _context2.next = 11;
+          return regeneratorRuntime.awrap(ConnectionRequest.findOne({
+            _id: requestId,
+            // Ensure the request ID is valid
+            toUserId: loginUserId,
+            // Ensure the request belongs to the logged-in user
+            status: "interested" // Ensure the request is in 'interested' status only if connection request is already accepted or rejected, then it will not allow again.
+
+          }));
+
+        case 11:
+          connectionRequest = _context2.sent;
+
+          if (connectionRequest) {
+            _context2.next = 14;
+            break;
+          }
+
+          return _context2.abrupt("return", res.status(404).json({
+            error: "Connection request not found or not in 'interested' status."
+          }));
+
+        case 14:
+          // Check status to ensure it can only be updated once
+
+          /*if (connectionRequest.status === "accepted" || connectionRequest.status === "rejected") {
+            return res.status(400).json({
+              error: `Connection request has already been ${connectionRequest.status}.`
+            });
+          }*/
+          // Update the connection request status
+          connectionRequest.status = status; // Set the new status
+
+          connectionRequest.updatedAt = Date.now(); // Update the timestamp
+
+          _context2.next = 18;
+          return regeneratorRuntime.awrap(connectionRequest.save());
+
+        case 18:
+          data = _context2.sent;
+          res.status(200).json({
+            message: "Connection request updated successfully to '".concat(status, "'."),
+            data: data
+          });
+          _context2.next = 26;
+          break;
+
+        case 22:
+          _context2.prev = 22;
+          _context2.t0 = _context2["catch"](0);
+          console.error("Error updating connection request:", _context2.t0);
+          res.status(500).send("Error updating connection request: " + _context2.t0.message);
+
+        case 26:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  }, null, null, [[0, 22]]);
+});
 module.exports = connectionRequestRouter; // Export the connection request router
